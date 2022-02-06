cbc7497f5d1261ba6ae4dabafc909b7cb307aa4d
Franco <franco.venezia@hotmail.com>
2022-02-06 13:08:33 -0300

    Casi todo listo, falta el tema diseño y limitar el tamaño del grid

---
+++ javascript.js
@@ -0,0 +1,39 @@
+const gridContainer = document.getElementById("gridContainer");
+const reseteo = document.getElementById("reseteo");
+let gridSize = prompt("Elige el tamaño de tu grid");
+
+createGrid(gridSize);
+
+function createDiv (size){
+    const div = document.createElement("div");
+    div.classList.add("divGrid");
+    div.classList.add("hoverxd");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    return div;
+}
+function createGrid (gridSize){
+    for (let i = 0; i<gridSize; i++){
+        for(let j = 0; j<gridSize; j++){
+            gridContainer.appendChild(createDiv(gridContainer.clientWidth / gridSize));
+        }
+        
+    }
+}
+gridContainer.addEventListener('mouseover', function (e) {
+    if (e.target.matches('.divGrid')) {
+      e.target.classList.add('active');
+    }
+  });
+
+  function reset() {
+    while (gridContainer.firstChild) {
+      gridContainer.removeChild(gridContainer.lastChild);
+    }
+    gridSize = prompt("Elige el tamaño de tu grid");
+    createGrid(gridSize);
+  }
+  reseteo.addEventListener('click', function () {
+    reset();
+  });
+  
